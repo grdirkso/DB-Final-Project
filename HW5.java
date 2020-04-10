cf4073c6d938041746728679861afd468e52159f
Gillian <grdirkso@uark.edu>
2020-04-09 19:31:02 -0500

    delete and insert on program strart

--- HW5.java
+++ HW5.java
@@ -16,6 +16,7 @@ public class HW5 {
 		String Password="G#Dirk$o223";
 		HW5 db = new HW5();
 		db.connect(Username, Password);
+		db.initDatabase(Username, Password, Username);
 	}
 
 	public void connect(String Username, String mysqlPassword) throws Exception {
@@ -27,5 +28,85 @@ public class HW5 {
             System.out.println("error");
 		throw e;
         }
+    }
+
+	// Execute an SQL query passed in as a String parameter
+    // and print the resulting relation
+    public void query(String q) {
+        try {
+            ResultSet resultSet = statement.executeQuery(q);
+            System.out.println("\n---------------------------------");
+            System.out.println("Query: \n" + q + "\n\nResult: ");
+            print(resultSet);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Print the results of a query with attribute names on the first line
+    // Followed by the tuples, one per line
+    public void print(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int numColumns = metaData.getColumnCount();
+
+        printHeader(metaData, numColumns);
+        printRecords(resultSet, numColumns);
+    }
+
+    // Print the attribute names
+    public void printHeader(ResultSetMetaData metaData, int numColumns) throws SQLException {
+        for (int i = 1; i <= numColumns; i++) {
+            if (i > 1)
+                System.out.print(",  ");
+            System.out.print(metaData.getColumnName(i));
+        }
+        System.out.println();
+    }
+
+    // Print the attribute values for all tuples in the result
+    public void printRecords(ResultSet resultSet, int numColumns) throws SQLException {
+        String columnValue;
+        while (resultSet.next()) {
+            for (int i = 1; i <= numColumns; i++) {
+                if (i > 1)
+                    System.out.print(",  ");
+                columnValue = resultSet.getString(i);
+                System.out.print(columnValue);
+            }
+            System.out.println("");
+        }
+    }
+
+    // Insert into any table, any values from data passed in as String parameters
+    public void insert(String table, String values) {
+        String query = "INSERT into " + table + " values (" + values + ")" ;
+        try {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Remove all records and fill them with values for testing
+    // Assumes that the tables are already created
+    public void initDatabase(String Username, String Password, String SchemaName) throws SQLException {
+        statement = connection.createStatement();
+		statement.executeUpdate("DELETE from ENROLLMENT");
+        statement.executeUpdate("DELETE from STUDENT");
+        statement.executeUpdate("DELETE from COURSE");
+		statement.executeUpdate("ALTER TABLE COURSE AUTO_INCREMENT = 101;");
+		statement.executeUpdate("ALTER TABLE ENROLLMENT AUTO_INCREMENT = 201;");
+
+        insert("STUDENT", "'010816228', 'CAROLINE GESCHWEND', 'COMPUTER SCIENCE'");
+        insert("STUDENT", "'010814651', 'GILLIAN DIRKSON', 'COMPUTER SCIENCE'");
+
+        insert("COURSE", " NULL,'CSCE', '4523', 'DATABASE', 3");
+        insert("COURSE", "NULL,'CSCE', '4813', 'GRAPHICS', 3");
+    
+        insert("ENROLLMENT", " NULL,'010814651', 101");
+		insert("ENROLLMENT", " NULL,'010814651', 102");
+        insert("ENROLLMENT", " NULL,'010816228', 101");
+        
     }
 }	
